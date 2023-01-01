f60b7f1a7ad81bb8356904827b69550c170f52d4
Manoekr <manobey34@gmail.com>
2023-01-01 16:37:13 +0300

    ViewComponent

--- Blog.WebUI/Areas/Admin/Controllers/HomeController.cs
+++ Blog.WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Blog.Business.Abstract;
+using Blog.Entities.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using static Blog.WebUI.ResultMessages.Messages;
 
 namespace Blog.WebUI.Areas.Admin.Controllers
 {
@@ -9,14 +14,19 @@ namespace Blog.WebUI.Areas.Admin.Controllers
     public class HomeController : Controller
     {
         private readonly IArticleService articleService;
+        private readonly UserManager<AppUser> userManager;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly ClaimsPrincipal _user;
 
-        public HomeController(IArticleService articleService)
+        public HomeController(IArticleService articleService,UserManager<AppUser> userManager)
         {
             this.articleService = articleService;
+            this.userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
             var articles = await articleService.GetAllArticlesWithCategoryNonDeletedAsync();
+            var loggedInUser = await userManager.GetUserAsync(HttpContext.User);
             return View(articles);
         }
     }
---
+++ Blog.WebUI/Areas/Admin/ViewComponents/DashboardHeaderViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Entities.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.WebUI.Areas.Admin.ViewComponents
+{
+    public class DashboardHeaderViewComponent : ViewComponent
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public DashboardHeaderViewComponent(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var loggendInUser = await userManager.GetUserAsync(HttpContext.User);
+            return View();
+        }
+    }
+}
